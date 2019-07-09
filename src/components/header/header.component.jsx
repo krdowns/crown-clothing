50a23500996827f27c05422a6f5d7d4ad099052b
Kenny Downs <kdowns@repairpal.com>
2019-07-09 14:02:22 -0700

    added sign in button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,6 +17,12 @@ const Header = () => (
       <Link className='option' to='/shop'>
         CONTACT
       </Link>
+      <Link className='option' to='/signin'>
+        SIGN IN
+      </Link>
+      <Link className='option' to='/shop'>
+        CART
+      </Link>
     </div>
   </div>
 );
